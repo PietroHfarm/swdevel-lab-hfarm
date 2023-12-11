cbdfbcbfd26ca7ce7dfce9ea7b428942fca7bae0
trapaki <angelaviolaano@gmail.com>
2023-12-11 17:11:25 +0100

    module splitting done
    division of the functions in multiple modules

--- backend/app/mymodules/csv_reading_function.py
+++ backend/app/mymodules/csv_reading_function.py
@@ -1,5 +1,4 @@
 import csv
-from geopy.distance import geodesic
 
 def leggi_dati_da_csv(file_path):
     dati = []
@@ -9,5 +8,4 @@ def leggi_dati_da_csv(file_path):
             dati.append(row)
     return dati
 
-def calcola_distanza(lat1, lon1, lat2, lon2):
-    return geodesic((lat1, lon1), (lat2, lon2)).meters
+
